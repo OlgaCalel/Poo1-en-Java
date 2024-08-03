7c3a8fb72f54edeeb4efeb58f97c25dc06e91e07
OlgaCalel <161219868+OlgaCalel@users.noreply.github.com>
2024-08-02 23:23:46 -0600

    POO1

---
+++ poo_1/src/modelo/Cliente.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author calel
+ */
+public class Cliente extends Persona {
+    private String nit;
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    @Override
+    public void agregar(){
+    System.out.println("Nit: "+getNit());
+    System.out.println("Nombres: " + this.getNombres());
+    System.out.println("Apellidos: " + this.getApellidos());
+    System.out.println("Direccion: " + this.getDireccion());
+    System.out.println("Telefono: " + this.getTelefono());
+    System.out.println("Fecha_nacimiento: " + getFecha_nacimiento());
+            }
+    
+}
